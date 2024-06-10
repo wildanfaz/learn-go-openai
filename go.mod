e53ebe2c86e8fdb598383025e4048a46b810f86b
Muhamad Wildan Faz <muhamadwildanfaz@gmail.com>
2024-06-10 14:03:38 +0700

    init: learn-go-openai

---
+++ go.mod
@@ -0,0 +1,41 @@
+module github.com/wildanfaz/learn-go-openai
+
+go 1.22.0
+
+require (
+	github.com/gofiber/contrib/websocket v1.3.1
+	github.com/gofiber/fiber/v2 v2.52.4
+	github.com/joho/godotenv v1.5.1
+	github.com/sashabaranov/go-openai v1.24.1
+	github.com/unidoc/unipdf/v3 v3.59.0
+)
+
+require (
+	github.com/andybalholm/brotli v1.1.0 // indirect
+	github.com/davecgh/go-spew v1.1.1 // indirect
+	github.com/fasthttp/websocket v1.5.9 // indirect
+	github.com/gabriel-vasile/mimetype v1.4.3 // indirect
+	github.com/google/uuid v1.6.0 // indirect
+	github.com/klauspost/compress v1.17.8 // indirect
+	github.com/mattn/go-colorable v0.1.13 // indirect
+	github.com/mattn/go-isatty v0.0.20 // indirect
+	github.com/mattn/go-runewidth v0.0.15 // indirect
+	github.com/pmezard/go-difflib v1.0.0 // indirect
+	github.com/rivo/uniseg v0.4.7 // indirect
+	github.com/savsgio/gotils v0.0.0-20240303185622-093b76447511 // indirect
+	github.com/sirupsen/logrus v1.9.3 // indirect
+	github.com/stretchr/testify v1.9.0 // indirect
+	github.com/unidoc/pkcs7 v0.2.0 // indirect
+	github.com/unidoc/timestamp v0.0.0-20200412005513-91597fd3793a // indirect
+	github.com/unidoc/unitype v0.4.0 // indirect
+	github.com/valyala/bytebufferpool v1.0.0 // indirect
+	github.com/valyala/fasthttp v1.54.0 // indirect
+	github.com/valyala/tcplisten v1.0.0 // indirect
+	golang.org/x/crypto v0.24.0 // indirect
+	golang.org/x/image v0.15.0 // indirect
+	golang.org/x/net v0.26.0 // indirect
+	golang.org/x/sys v0.21.0 // indirect
+	golang.org/x/text v0.16.0 // indirect
+	golang.org/x/xerrors v0.0.0-20231012003039-104605ab7028 // indirect
+	gopkg.in/yaml.v3 v3.0.1 // indirect
+)
---
+++ main.go
@@ -0,0 +1,240 @@
+package main
+
+import (
+	"bytes"
+	"context"
+	"io"
+	"log"
+	"os"
+	"path/filepath"
+	"strings"
+
+	"github.com/gofiber/contrib/websocket"
+	"github.com/gofiber/fiber/v2"
+	_ "github.com/joho/godotenv/autoload"
+	"github.com/sashabaranov/go-openai"
+	"github.com/unidoc/unipdf/v3/common/license"
+	"github.com/unidoc/unipdf/v3/extractor"
+	"github.com/unidoc/unipdf/v3/model"
+)
+
+func init() {
+	// Make sure to load your metered License API key prior to using the library.
+	// If you need a key, you can sign up and create a free one at https://cloud.unidoc.io
+	err := license.SetMeteredKey(os.Getenv(`UNIDOC_LICENSE_API_KEY`))
+	if err != nil {
+		panic(err)
+	}
+}
+
+func main() {
+	initRouter()
+}
+
+
+
+func initRouter() {
+	app := fiber.New()
+
+	app.Post("/learn/go-openai", handler)
+
+	app.Use("/ws", func(c *fiber.Ctx) error {
+		// IsWebSocketUpgrade returns true if the client
+		// requested upgrade to the WebSocket protocol.
+		if websocket.IsWebSocketUpgrade(c) {
+			c.Locals("allowed", true)
+			return c.Next()
+		}
+		return fiber.ErrUpgradeRequired
+	})
+
+	app.Get("/ws/:id", websocket.New(func(c *websocket.Conn) {
+		// websocket.Conn bindings https://pkg.go.dev/github.com/fasthttp/websocket?tab=doc#pkg-index
+		var (
+			content string
+			mt  int
+			msg []byte
+			err error
+		)
+
+		pdftext, err := outputPdfText("./cerita-singkat.pdf")
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		content += "this is pdf text: " + pdftext
+		content += "\n"
+
+		openaiClient := initOpenAI()
+
+		for {
+			if mt, msg, err = c.ReadMessage(); err != nil {
+				log.Println("read:", err)
+				break
+			}
+
+			content += "user prompt: " + string(msg) + "\n"
+
+			resp, err := openaiClient.CreateChatCompletion(context.Background(), openai.ChatCompletionRequest{
+				Model: openai.GPT3Dot5Turbo,
+				Messages: []openai.ChatCompletionMessage{
+					{
+						Role:    openai.ChatMessageRoleUser,
+						Content: content,
+					},
+				},
+			})
+
+			if err != nil {
+				log.Println(err)
+				break
+			}
+
+			if err = c.WriteMessage(mt, []byte(resp.Choices[0].Message.Content)); err != nil {
+				log.Println("write:", err)
+				break
+			}
+		}
+	}))
+
+	app.Listen(":3000")
+}
+
+func handler(c *fiber.Ctx) error {
+	fileHeader, err := c.FormFile("pdf")
+	if err != nil {
+		return c.Status(fiber.StatusInternalServerError).JSON(fiber.Map{
+			"message": err.Error(),
+		})
+	}
+
+	contentType := fileHeader.Header.Get("Content-Type")
+	if contentType != "application/pdf" {
+		return c.Status(fiber.StatusBadRequest).JSON(fiber.Map{
+			"message": "Only PDF file are allowed",
+		})
+	}
+
+	if !hasPDFExtension(fileHeader.Filename) {
+		return c.Status(fiber.StatusBadRequest).JSON(fiber.Map{
+			"message": "File extension not supported",
+		})
+	}
+
+	file, err := fileHeader.Open()
+	if err != nil {
+		return c.Status(fiber.StatusInternalServerError).JSON(fiber.Map{
+			"message": err.Error(),
+		})
+	}
+	defer file.Close()
+
+	fileByte, err := io.ReadAll(file)
+	if err != nil {
+		return c.Status(fiber.StatusInternalServerError).JSON(fiber.Map{
+			"message": err.Error(),
+		})
+	}
+
+	pdfReader, err := model.NewPdfReader(bytes.NewReader(fileByte))
+	if err != nil {
+		return c.Status(fiber.StatusInternalServerError).JSON(fiber.Map{
+			"message": err.Error(),
+		})
+	}
+
+	numPages, err := pdfReader.GetNumPages()
+	if err != nil {
+		return c.Status(fiber.StatusInternalServerError).JSON(fiber.Map{
+			"message": err.Error(),
+		})
+	}
+
+	allText := ""
+
+	for i := 1; i <= numPages; i++ {
+		page, err := pdfReader.GetPage(i)
+		if err != nil {
+			return c.Status(fiber.StatusInternalServerError).JSON(fiber.Map{
+				"message": err.Error(),
+			})
+		}
+
+		ex, err := extractor.New(page)
+		if err != nil {
+			return c.Status(fiber.StatusInternalServerError).JSON(fiber.Map{
+				"message": err.Error(),
+			})
+		}
+
+		text, err := ex.ExtractText()
+		if err != nil {
+			return c.Status(fiber.StatusInternalServerError).JSON(fiber.Map{
+				"message": err.Error(),
+			})
+		}
+
+		allText += text
+		allText += "\n"
+	}
+
+	// Expected : save content to db
+
+	return c.Status(fiber.StatusOK).JSON(fiber.Map{
+		"message": "Success",
+		"data": allText,
+	})
+}
+
+func hasPDFExtension(filename string) bool {
+    return strings.ToLower(filepath.Ext(filename)) == ".pdf"
+}
+
+func initOpenAI() *openai.Client {
+	return openai.NewClient(os.Getenv("OPENAI_TOKEN"))
+}
+
+// outputPdfText prints out contents of PDF file to stdout.
+func outputPdfText(inputPath string) (string, error) {
+	f, err := os.Open(inputPath)
+	if err != nil {
+		return "", err
+	}
+
+	defer f.Close()
+
+	pdfReader, err := model.NewPdfReader(f)
+	if err != nil {
+		return "", err
+	}
+
+	numPages, err := pdfReader.GetNumPages()
+	if err != nil {
+		return "", err
+	}
+
+	allText := ""
+	for i := 0; i < numPages; i++ {
+		pageNum := i + 1
+
+		page, err := pdfReader.GetPage(pageNum)
+		if err != nil {
+			return "", err
+		}
+
+		ex, err := extractor.New(page)
+		if err != nil {
+			return "", err
+		}
+
+		text, err := ex.ExtractText()
+		if err != nil {
+			return "", err
+		}
+
+		allText += text
+		allText += "\n"
+	}
+
+	return allText, nil
+}
